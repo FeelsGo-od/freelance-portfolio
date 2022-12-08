1d277df9660f643978de74c3cdc67baa646e633c
FeelsGo-od <andrew.sokolowsky@gmail.com>
2022-12-08 22:11:42 +0100

    add node routes and email form

---
+++ app.js
@@ -0,0 +1,88 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import express from 'express';
+import nodemailer from 'nodemailer';
+const app = express();
+const router = express.Router();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(__dirname + '/'));
+
+import bodyParser from 'body-parser';
+router.use(bodyParser.json());
+router.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+router.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '/about.html'));
+});
+
+router.get('/projects', (req, res) => {
+  res.sendFile(path.join(__dirname, '/projects.html'));
+});
+
+router.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, '/contact.html'));
+});
+
+router.get('/contact_success', (req, res) => {
+  res.sendFile(path.join(__dirname, '/contact_success.html'));
+});
+
+router.post('/contact', function (req, res) {
+  try {
+    const recipient = req.body.email;
+    console.log('recipient:', recipient);
+
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+      },
+    });
+
+    let mailOptions = {
+      from: 'andrewdevvv@gmail.com',
+      to: 'andrew.sokolowsky@gmail.com',
+      subject: `Message from ${recipient}`,
+      text: 'Hi there, I want to get in touch with you :)',
+    };
+
+    transporter.sendMail(mailOptions, function (err, data) {
+      if (err) {
+        console.log('Error ' + err);
+      } else {
+        console.log('Email sent successfully');
+        res.sendFile(path.join(__dirname, '/contact_success'));
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    return res.send('Error uploading file');
+  }
+});
+
+app.use('/', router);
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
