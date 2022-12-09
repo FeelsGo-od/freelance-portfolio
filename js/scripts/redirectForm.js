92e928bb097a82d7974596f939ebc143c3f69d12
FeelsGo-od <andrew.sokolowsky@gmail.com>
2022-12-09 09:02:18 +0100

    change form submit path

--- js/scripts/redirectForm.js
+++
@@ -1,7 +0,0 @@
-export default function redirectForm() {
-  document.querySelectorAll('form').forEach((form) => {
-    form.addEventListener('submit', function () {
-      window.location.href = '/contact_success.html';
-    });
-  });
-}
