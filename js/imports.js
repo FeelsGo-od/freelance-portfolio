d5ff2a37ec884fd6cb9ae35a15d1d034a5e006de
FeelsGo-od <andrew.sokolowsky@gmail.com>
2022-12-19 15:03:11 +0100

    fix contact form & add social link

--- js/imports.js
+++ js/imports.js
@@ -2,8 +2,10 @@ import header from './scripts/header.js';
 import aboutScroll from './scripts/about_scroll.js';
 import downScroll from './scripts/downScroll.js';
 import portfolioPopup from './scripts/portfolio_popup.js';
+import emailForm from './scripts/email_form.js';
 
 header();
 aboutScroll();
 downScroll();
 portfolioPopup();
+emailForm();
---
+++ js/scripts/email_form.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('#form');
+
+export default function emailForm() {
+  if (!form) return;
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let email = document.querySelector('#email').value;
+    let myText = `${email} wants to work with you ;)`;
+
+    let token = '5802490856:AAHz3o_W0SKEd3W5vaVcjeelRkfzU1NNjp4';
+    let chat_id = -618226943;
+    let url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${myText}`;
+
+    let api = new XMLHttpRequest();
+    api.open('GET', url, true);
+    api.send();
+
+    window.location.href =
+      window.location.protocol +
+      '//' +
+      window.location.host +
+      '/contact_success.html';
+  });
+}
